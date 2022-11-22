92b9e4f086e062ede89fe62ac1258b94e963da9c
zzvsjs1 <benzzvsjs@gmail.com>
2022-11-23 09:52:46 +1100

    Fix typo

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -68,7 +68,7 @@ static struct {
 	const char *name;
 	const char *description;
 	int (*handler) (char *);
-} cmd_table [] =
+} cmd_table[] =
 {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
