3443d87068edb58c9922ea9de9def2884d004f76
zzvsjs <benzzvsjs@gmail.com>
2025-04-07 23:54:19 +1000

    Patch native div and mul files

--- abstract-machine/scripts/riscv32-nemu.mk
+++ abstract-machine/scripts/riscv32-nemu.mk
@@ -5,4 +5,4 @@ CFLAGS  += -DISA_H=\"riscv/riscv.h\"
 AM_SRCS += riscv/nemu/start.S \
            riscv/nemu/cte.c \
            riscv/nemu/trap.S \
-           riscv/nemu/vme.c
+           riscv/nemu/vme.c 
