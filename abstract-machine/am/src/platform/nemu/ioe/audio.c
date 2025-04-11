0ee2607b592424174b1379da49eb4dc500e8a638
zzvsjs <benzzvsjs@gmail.com>
2025-04-12 00:38:08 +1000

    Add some audio support code

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -8,19 +8,27 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
-void __am_audio_init() {
+void __am_audio_init() 
+{
+  
 }
 
-void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
+void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) 
+{
   cfg->present = false;
 }
 
-void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) 
+{
+
 }
 
-void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
+void __am_audio_status(AM_AUDIO_STATUS_T *stat) 
+{
   stat->count = 0;
 }
 
-void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+void __am_audio_play(AM_AUDIO_PLAY_T *ctl) 
+{
+
 }
