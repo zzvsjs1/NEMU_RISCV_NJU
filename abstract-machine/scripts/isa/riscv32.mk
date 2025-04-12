14846a34786d9a7051032e91a236f80567075ac9
zzvsjs <benzzvsjs@gmail.com>
2025-04-12 12:44:30 +1000

    Continue on audio sub system

--- abstract-machine/scripts/isa/riscv32.mk
+++ abstract-machine/scripts/isa/riscv32.mk
@@ -1,4 +1,5 @@
-CROSS_COMPILE := riscv32-unknown-linux-gnu-
+# CROSS_COMPILE := riscv32-unknown-linux-gnu-
+CROSS_COMPILE := riscv32-unknown-elf-
 COMMON_FLAGS  := -fno-pic -march=rv32i_zicsr -mabi=ilp32
 CFLAGS        += $(COMMON_FLAGS) -static
 ASFLAGS       += $(COMMON_FLAGS) -O0
